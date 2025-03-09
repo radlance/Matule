672a22e3fd97ded91c335d98c8b902f33920f97f
Dmitry <manyakindima@gmail.com>
2025-03-09 21:36:35 +0300

    remove useless repository & update dependency versions

--- app/src/main/java/com/radlance/matule/domain/history/HistoryRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.radlance.matule.domain.history
-
-import com.radlance.matule.domain.remote.FetchResult
-
-interface HistoryRepository {
-    suspend fun loadHistory(): FetchResult<List<HistoryProduct>>
-}
